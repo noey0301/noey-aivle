28bff9a0096a66b15febf9b2832b7ca208be3b6a
noey0301 <112459620+noey0301@users.noreply.github.com>
2023-01-03 17:37:02 +0900

    Add files via upload

--- pro.py
+++ pro.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-
+!pip install matplotlib
 import matplotlib.pyplot as plt
 # import matplotlib
 # matplotlib.use('Agg')
